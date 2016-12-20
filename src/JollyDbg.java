0a111b14cb794305c518583be370e845a422e3d2
Stephen Dunn <stephen@setemtech.com>
2016-12-20 11:55:56 -0500

    version updated to reflect register display working

--- src/JollyDbg.java
+++ src/JollyDbg.java
@@ -28,7 +28,7 @@ import java.util.stream.Stream;
 
 public class JollyDbg extends Application
 {
-  public static final String VERSION = "1.0.0";
+  public static final String VERSION = "1.0.1";
 
   private Thread                   gdbThread    = null;
   private TextArea                 txtAssembly  = null;
